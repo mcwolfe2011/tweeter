106b080e61ce11c4454732a1919bf5f58ce34e91
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-28 18:22:01 -0700

    Added conditional to composer-char-counter and more css styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
     var charCount = $(this).val().length;
     var charLeft = 140 - charCount;
     $('#charCount').text(charLeft);
+    if (charLeft < 0) {
+      $('#charCount').addClass('red');
+    } else {
+      $('#charCount').removeClass('red');
+    }
   });
 });
+
+
+
+
 
 
 
